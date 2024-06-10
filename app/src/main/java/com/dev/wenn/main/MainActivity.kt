dc5719b31e65e24d4562814f2e168107142a6d96
Wenn <yanto.wendy98@gmail.com>
2024-06-10 11:55:20 +0800

    setup main activity

--- app/src/main/java/com/dev/wenn/main/MainActivity.kt
+++ app/src/main/java/com/dev/wenn/main/MainActivity.kt
@@ -2,10 +2,12 @@ package com.dev.wenn.main
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.dev.wenn.R
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
     }
 }
