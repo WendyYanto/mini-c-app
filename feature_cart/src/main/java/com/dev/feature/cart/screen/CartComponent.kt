aae415ae7ad56ce194d758f9e0a1d1bdcc9491fc
Wenn <yanto.wendy98@gmail.com>
2024-09-10 20:01:25 +0800

    clean up

--- feature_cart/src/main/java/com/dev/feature/cart/screen/CartComponent.kt
+++ feature_cart/src/main/java/com/dev/feature/cart/screen/CartComponent.kt
@@ -3,13 +3,11 @@ package com.dev.wenn.main.screen
 import androidx.appcompat.app.AppCompatActivity
 import com.dev.core.scope.ActivityScope
 import com.dev.core.scope.AppScope
-import com.dev.core.scope.FeatureScope
 import com.squareup.anvil.annotations.ContributesTo
 import com.squareup.anvil.annotations.MergeSubcomponent
 import dagger.BindsInstance
 import dagger.Subcomponent
 
-@FeatureScope
 @MergeSubcomponent(
     scope = ActivityScope::class
 )
