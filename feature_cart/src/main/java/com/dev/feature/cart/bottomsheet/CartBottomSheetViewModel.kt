a3707efc0f496f7689191002240f2d565abdc8bb
Wenn <yanto.wendy98@gmail.com>
2024-09-16 13:35:49 +0800

    clean up

--- feature_cart/src/main/java/com/dev/feature/cart/bottomsheet/CartBottomSheetViewModel.kt
+++ feature_cart/src/main/java/com/dev/feature/cart/bottomsheet/CartBottomSheetViewModel.kt
@@ -1,13 +1,8 @@
 package com.dev.feature.cart.bottomsheet
 
 import androidx.lifecycle.ViewModel
-import com.dev.core.scope.ActivityScope
 import com.dev.domain.cart.DomainCartTextProvider
-import com.squareup.anvil.annotations.ContributesBinding
 
-@ContributesBinding(
-    scope = ActivityScope::class
-)
 class CartBottomSheetViewModel(
     private val textProvider: DomainCartTextProvider
 ) : ViewModel() {
